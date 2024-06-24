cd0e13b944d4f9b6c5c3b39a2b5e2c0535ddd882
Magnus Hagdorn <magnus.hagdorn@charite.de>
2024-06-24 12:33:26 +0200

    style: make pylint happy

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -6,16 +6,16 @@ import argparse
 from inflammation import models, views
 
 
-def main(args):
+def main(arguments):
     """The MVC Controller of the patient inflammation data system.
 
     The Controller is responsible for:
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    in_files = args.infiles
+    in_files = arguments.infiles
     if not isinstance(in_files, list):
-        in_files = [args.infiles]
+        in_files = [arguments.infiles]
 
 
     for filename in in_files:
