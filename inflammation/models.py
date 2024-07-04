33a41c3d19030214fab092183282452d678dc3df
andreas <andreas.reppas@charite.de>
2024-07-04 13:20:59 +0200

    Add changes for testing

--- inflammation/models.py
+++ inflammation/models.py
@@ -40,3 +40,25 @@ def daily_min(data):
     :param data: a 2D data array
     :returns: the min"""
     return np.min(data, axis=0)
+
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
+
+    if len(data.shape) != 2:
+        raise ValueError('Data should be a 2-Dimensional array')
+
+    setup_max = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / setup_max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
